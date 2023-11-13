b3310ca3b3ba45a383bd8386e8ad66cbe612ec21
Mellissa Santos <mellissa03santos@gmail.com>
2023-11-13 00:14:28 -0300

    localStorage

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 const elementsLi = document.querySelectorAll("li");
 const inputElement = document.getElementById('input');
 const myWebpage = document.getElementById('letterboxd');
+const firstLi = document.getElementById('first-li');
+const secondLi = document.getElementById('second-li');
+const thirdLi = document.getElementById('third-li');
+let id = "";
+
+firstLi.innerText = JSON.parse(localStorage.getItem('movie-first-li')) || "Primeiro filme preferido.";
+secondLi.innerText = JSON.parse(localStorage.getItem('movie-second-li')) || "Segundo filme preferido.";
+thirdLi.innerText = JSON.parse(localStorage.getItem('movie-third-li')) || "Terceiro filme preferido.";
 
 // Função que adiciona a classe 'tech' ao elemento `li` quando este for clicado.
-
 function addClassTech (event) {
     document.querySelector(".tech").classList.remove("tech");
     event.target.classList.add("tech");
+    id = event.target.id;
 }
 
 for(index = 0; index < elementsLi.length; index += 1) {
     elementsLi[index].addEventListener('click', addClassTech);
 }
 
-// Função que ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech'.
+// Função que ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech' e envia para o localStorage.
 function movieName(event) {
     document.querySelector(".tech").innerText = event.target.value;
+    localStorage.setItem(`movie-${id}`, JSON.stringify(event.target.value));
 }
 
 inputElement.addEventListener('input', movieName);
